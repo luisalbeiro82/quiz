c497cf66f0aaa40a27f875333920c8a1b17ffa06
luisalbeiro82 <luis.albeiro.h@gmail.com>
2015-07-15 20:59:17 -0500

    autoload
    tema 5 autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,21 +1,37 @@
 
 var models = require('../models/models.js');
+//autoload - factoriza el codigo si ruta incluye: quizId
+exports.load=function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+				req.quiz=quiz;
+				next();
+			}else{
+				next(new Error('No existe quizid='+ quizId));
+			}
+		}
+	).catch(function(error){next (error);})
+};
+//GET  /quizes
+exports.index=function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes:quizes});
+	}).catch(function(error){next (error);})
+};
 
 //GET  /quizes/question
-exports.question=function(req,res){
-	models.Quiz.findAll().then(function(quiz){
-		res.render('quizes/question',{pregunta:quiz[0].pregunta})
-		})
-	};
-exports.answer=function(req,res){
-	models.Quiz.findAll().then(function(quiz){
-		if(req.query.respuesta===quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta:'Correcto'});
-		}else{
-			res.render('quizes/answer',{respuesta:'Incorrecto'});
-		}
-	})
+exports.show=function(req,res){
+		res.render('quizes/show',{quiz:req.quiz});
 };
+exports.answer=function(req,res){
+	var resultado='Incorrecto';
+		if(req.query.respuesta===req.quiz.respuesta){
+			resultado='Correcto';
+		}
+			res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado});
+};
+
 exports.autor=function(req,res){
 	res.render('autor',{nombre:'Luis albeiro Hernandez'});
 	//res.render('quizes/autor',{nombre:'Capital de Italia'});
--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,11 @@ var quizController=require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+router.param('quizId',quizController.load);//autoload :quizId
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+//definicion de rutas de quises
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/autor',quizController.autor);
-//router.get('/autor/autor',quizController.autor);
 module.exports = router;
